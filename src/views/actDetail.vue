1665b7615280b8ee52182da19773b3af75d667ca
sayoriqwq <2531600563@qq.com>
2024-02-23 23:01:10 +0800

    feat: html也可以解析到图片了，问题是会发出错误请求，不知道怎么改

--- src/views/actDetail.vue
+++ src/views/actDetail.vue
@@ -14,6 +14,10 @@ import { getActByIdApi } from '@/apis/activity'
 import { onMounted, ref } from 'vue'
 import { Activity } from '@/types'
 import { useRoute } from 'vue-router'
+import { getPicApi } from '@/apis/user'
+import { useFileConversion } from '@/hooks/useFileConversion'
+
+const [fileToBase64] = useFileConversion()
 
 const route = useRoute()
 
@@ -26,7 +30,28 @@ const getActInfo = async () => {
   if (!e && r) {
     const { result } = r
     activity.value = result
+    activity.value.content = await parseImg(result.content)
   }
+}
+
+const parseImg = async (htmlStr: string | undefined) => {
+  if (!htmlStr) return
+  let outputStr = htmlStr
+  const imgRegex = /<img.*?src=["'](.*?)["']/g
+  let match
+  // 使用正则表达式进行匹配，并提取 src 属性值
+  while ((match = imgRegex.exec(htmlStr)) !== null) {
+    const src = match[1]
+    try {
+      const res = await getPicApi(src)
+      //将匹配到的src字段改为对应的base64数据
+      const base64src = await fileToBase64(res.data, '')
+      outputStr = outputStr.replace(match[0], `<img src="${base64src}"`)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+  return outputStr
 }
 
 onMounted(() => {
